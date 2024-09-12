3c25cb08d759f9b4e06ae98d96cfaa30e47ac56e
HANSTZ3/HANS-MD <mpyabottzhans@gmail.com>
2024-09-12 14:59:34 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# HANSTZ-Session-Generator
+- Kindly star my repo
+- Fork and edit as you wish
+- Deploy to your favourite hosting server eg Heroku or Render or self hosting
+
+<strong>NB:<strong/> This repo also generates session ID for all bots using whiskeysockets/baileys
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+<br/>QR- WEB - PAIR CODE FOR BOT WITH WHISKEYSOCKETS/BAILEYS
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+<p align="center">
+   <a href="https://github.com/HANSTZ3/HANSTZ">
+</a>
+ <p align="center"><img src="https://profile-counter.glitch.me/{HANSTZ}/count.svg" alt="HANSTZ:: Visitor's Count" /></p>
+
+
+
+[`ℹ️Contact Owner`](https://wa.me/message/UTQ7TX2M7TDYM1)
+
+[!`FORK KINGFREDIE`](https://github.com/HANSTZ3/HANSTZ) 
+
+Now Deploy
+    <br>
+<a href='https://dashboard.heroku.com/new?template=https://github.com/HANSTZ3/HANSTZ' target="_blank"><img alt='DEPLOY' src='https://img.shields.io/badge/-DEPLOY-black?style=for-the-badge&logo=heroku&logoColor=black'/>
+
+
+ <a href="https://github.com/HANSTZ3"><img src="https://github.com/HANSTZ3.png" width="250" height="250" alt="Cod3Uchiha"/></a>
+
---
+++ pair.js
@@ -0,0 +1,113 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: Gifted_Tech,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function GIFTED_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair_Code_By_Gifted_Tech = Gifted_Tech({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Linux)", "", ""]
+             });
+             if(!Pair_Code_By_Gifted_Tech.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_Gifted_Tech.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_Gifted_Tech.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Gifted_Tech.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
+
+               let GIFTED_MD_TEXT = `
+*_Pair Code Connected*
+*_Made With by Hans Tech♻️_*
+❖⊷━━━━━━━━━━━━━━━━━━━━━━⊷❖
+╭──━━━━━━◇✣❖◇━━━━━━━───❂
+┃ FOR ALL HANS_MD v1 BOT
+┃ _Thanks for your support._
+╰──━━━━━━◇✣❖◇━━━━━━━───❂
+  ▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒
+╭──━━━━━━◇✣❖◇━━━━━━━───❂
+┃❖  『••• ⬡𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽⬡ •••』
+┃⬡ *Ytube:* _youtube.com/@freeonlinetvT1_
+┃⬡ *wa-me:* _https://wa.me/255692540143_
+┃⬡ *wa-me:* _https://wa.me/255692540143_
+┃⬡ *t-me:* _https://t.me/freditech_
+┃
+┃⬡MY bot Repos⬡
+┃
+┃⬡ *HANS bot:* _https://github.com/HANSTZ3/HANS-MD_
+┃⬡ *My Github:* _https://github.com/HANSTZ3_
+┃
+┃⬡Support channels⬡
+┃
+┃⬡ *WATSAPP GROUP:* _https://chat.whatsapp.com/Ia4V8wnwCIjLYaVI3FVHb4_
+┃⬡ *WATSAPP:* _https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f_
+┃⬡ *TELEGRAM:* _https://t.me/+u3zlb5y6OfxhOTdk_
+┃
+┃⬡ *Made in Tanzania by* _Hanstz|HansTech
+╰───━━━━━━◇✣❖◇━━━━━━━───❂
+▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒▓▒
+✣➻⊷━━━━━━━━━━━━━━━━━━━━━⊷➻✣
+
+_Don't Forget To Give Star To My Repo_`
+ await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_Gifted_Tech.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    GIFTED_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
+        }
+    }
+    return await GIFTED_MD_PAIR_CODE()
+});
+module.exports = router
